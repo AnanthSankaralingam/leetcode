2af9d60a62c266889e83fb02d9ea769e9e4d1539
Ananth Sankaralingam <79415395+AnanthSankaralingam@users.noreply.github.com>
2023-10-04 11:19:05 -0400

    Time: 2 ms (86.30%), Space: 45.2 MB (75.28%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        //since array is sorted, two pointers is more 
+        //efficient than hashmap
+        //left pointer increased when sum less than target
+        //right pointer decremented when sum greater
+        int left=0, right = numbers.length-1;
+        while(left<right){
+            if(numbers[left] + numbers[right] == target){
+                return new int[]{left+1,right+1};
+            }else if(numbers[left]+numbers[right] > target){
+                right--;
+            }else{
+                left++;
+            }
+        } 
+        return null;
+    }
+}
