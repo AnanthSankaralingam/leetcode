d98c1910e4541e9d87db3e3cd9ad5b181c0ae059
Ananth Sankaralingam <79415395+AnanthSankaralingam@users.noreply.github.com>
2023-10-15 14:34:12 -0400

    Time: 0 ms (100.00%), Space: 43.6 MB (42.56%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -0,0 +1,30 @@
+import java.util.*;
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        //runner walker 
+        ListNode walker = head, runner = head;
+        if(head == null){
+            return false;
+        }
+        while(runner != null && runner.next != null){
+            walker = walker.next;
+            runner = runner.next.next;
+            if(walker == runner){
+                return true;
+            }
+            
+        }
+        return false;
+    }
+}
