f1e976d80c16b9d25ac76f4d0ec9e31e573e128a
Ananth Sankaralingam <79415395+AnanthSankaralingam@users.noreply.github.com>
2024-03-20 23:53:16 -0400

    Time: 48 ms (50.68%), Space: 128.1 MB (6.00%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.java
@@ -0,0 +1,90 @@
+class LRUCache {
+    /*Since we need to evict the LRU at an exceeded capacity and need to keep track
+      of which nodes were referenced recently, use a doubly linked list. Right pointer
+      (head) is most recent, left pointer is least recent. Create helper methods to add
+      and remove nodes to the list. Maintain a hashmap of nodes for O(1) operations and
+      tail/head reference for O(1) updates. */
+    private Map<Integer, Node> cache;
+    private int capacity;
+
+    private Node left;
+    private Node right;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        cache = new HashMap<>();
+
+        //left = LRU , right = most recent
+        this.left = new Node(0, 0);
+        this.right = new Node(0, 0);
+        this.left.next = this.right;
+        this.right.prev = this.left;
+    }
+
+    public int get(int key) {
+        if (cache.containsKey(key)) {
+            remove(cache.get(key));
+            insert(cache.get(key));
+            return cache.get(key).val;
+        } else {
+            return -1;
+        }
+    }
+
+    public void put(int key, int value) {
+        if (cache.containsKey(key)) {
+            remove(cache.get(key));
+        }
+        cache.put(key, new Node(key, value));
+        insert(cache.get(key));
+
+        if (cache.size() > capacity) {
+            // remove from the list and delte the LRU from the hashmap
+            Node lru = this.left.next;
+            remove(lru);
+            cache.remove(lru.key);
+        }
+    }
+
+    // remove node from list
+    public void remove(Node node) {
+        Node prev = node.prev;
+        Node next = node.next;
+
+        prev.next = next;
+        next.prev = prev;
+    }
+
+    // insert node at right
+    public void insert(Node node) {
+        Node prev = this.right.prev;
+        Node next = this.right;
+
+        prev.next = node;
+        next.prev = node;
+
+        node.next = next;
+        node.prev = prev;
+    }
+
+    private class Node {
+
+        private int key;
+        private int val;
+
+        Node next;
+        Node prev;
+
+        public Node(int key, int val) {
+            this.key = key;
+            this.val = val;
+        }
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
