2f899057a0c87445f0690dd18ca67af70a6bf5f1
Ananth Sankaralingam <79415395+AnanthSankaralingam@users.noreply.github.com>
2024-06-09 22:35:21 -0400

    Time: 6 ms (97.82%), Space: 47.4 MB (83.81%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,17 +1,22 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+        //time: O(n * klog(k)) k is max str length
+        //space: O(n * k) n unique keys of length k
+        //map - key anagram, value original string
         Map<String, List<String>> map = new HashMap<>();
-        //key is string, value is anagrams
-        for(int i=0; i<strs.length; i++){
-            char[] c = strs[i].toCharArray();
+        for(String str : strs){
+            char []c = str.toCharArray(); //sort letters to get anagram
             Arrays.sort(c);
-            String key = new String(c);
-            if(!map.containsKey(key)){
-                map.put(key, new ArrayList<>());
+            String key = new String(c); 
+            if(map.containsKey(key)){
+                map.get(key).add(str); //append to list
+            }else{
+                List<String> list= new ArrayList<>(); //create new list
+                list.add(str);
+                map.put(key,list);
             }
-            map.get(key).add(strs[i]);
         }
-        return new ArrayList<>(map.values());
+        
+        return new ArrayList<>(map.values()); //use parenthesis to convert array to arraylist
     }
-    
 }
